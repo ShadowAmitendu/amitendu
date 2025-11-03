a25d61a90932220bcdd725d319ff8c33330f4341
Amitendu Bikash Dhusiya <amitendubikashdhusiya@gmail.com>
2025-11-03 21:19:31 +0530

    added header and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
+import { Blog } from './blog/blog';
+import { Pagenotfound } from './pagenotfound/pagenotfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+    title: 'Amitendu Bikash Dhusiya',
+  },
+  {
+    path: 'home',
+    component: Home,
+    title: 'Home',
+  },
+  {
+    path: 'about',
+    component: About,
+    title: 'About',
+  },
+  {
+    path: 'projects',
+    component: Projects,
+    title: 'Projects',
+  },
+  {
+    path: 'blog',
+    component: Blog,
+    title: 'Blog',
+  },
+  {
+    path: '**',
+    component: Pagenotfound,
+    title: 'PAGE NOT FOUND ERROR',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { Header } from './header/header';
+import { Navbar } from './navbar/navbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Navbar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
